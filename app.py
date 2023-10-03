98e3cbe7665d1957c6e877f21b88fe072084c4a8
DivakarChakali <117016431+DivakarChakali@users.noreply.github.com>
2023-10-03 06:35:54 +0000

    updated templates and app.py file

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def defalt():
-  return "<h1> this is default</h1>"
+def home():
+  return render_template('home.html')
+
+
+@app.route('/about')
+def about():
+  return render_template('about.html')
 
 
 if __name__ == '__main__':
