4a775f4a0583fd9e3026b8ebbf0e665f588aef3e
DivakarChakali <117016431+DivakarChakali@users.noreply.github.com>
2023-10-12 06:23:05 +0000

    made functional and aesthetic improvements

--- app.py
+++ app.py
@@ -1,11 +1,22 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
-import re
+from flask_mail import Mail, Message
+import re, os
 
 app = Flask(__name__)
+mail = Mail(app)
 app.secret_key = 'BD9C8CDB5A98EEDA'
 
 # Simple email validation pattern
 email_pattern = r'^[\w\.-]+@[\w\.-]+$'
+
+#app configuratin for the mail sending
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME')
+app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD')
+# app.config['MAIL_DEFAULT_SENDER'] = 'divakarpackers@gmail.com'
+app.config['MAIL_USE_TLS'] = False
+app.config['MAIL_USE_SSL'] = True
 
 
 @app.route('/')
@@ -16,11 +27,6 @@ def home():
 @app.route('/services')
 def services():
   return render_template('services.html')
-
-
-@app.route('/thank_you')
-def thank_you():
-  return 'Thank you for your submission!'
 
 
 @app.route('/aboutus')
@@ -63,6 +69,7 @@ def submit_quote():
     return redirect(url_for('requestquote'))
 
   # Save the data to a database or perform further processing here
+
   # For this example, we'll just print the data
   print(f'Name: {name}')
   print(f'Email: {email}')
@@ -74,6 +81,75 @@ def submit_quote():
 
   flash('Quote request submitted successfully', 'success')
   return redirect(url_for('requestquote'))
+
+
+quotes = []
+
+
+@app.route('/submit', methods=['POST'])
+def submit():
+  # Get form data
+  name = request.form['name']
+  email = request.form['email']
+  phone = request.form['phone']
+  moving_date = request.form['moving_date']
+  origin = request.form['origin']
+  destination = request.form['destination']
+  special_requests = request.form['special_requests']
+
+  # Server-side validation
+  email_pattern = r'^[\w\.-]+@[\w\.-]+\.\w+$'
+  phone_pattern = r'^\d{10}$'
+  errors = []
+
+  if not name:
+    errors.append('Name is required')
+  if not re.match(email_pattern, email):
+    errors.append('Invalid email format')
+  if not re.match(phone_pattern, phone):
+    errors.append('Invalid phone number format')
+  if not moving_date:
+    errors.append('Moving date is required')
+
+  if errors:
+    return render_template('home.html', errors=errors)
+  else:
+    # Store the data or send it to your backend
+    quotes.append({
+        'name': name,
+        'email': email,
+        'phone': phone,
+        'moving_date': moving_date,
+        'origin': origin,
+        'destination': destination,
+        'special_requests': special_requests
+    })
+    subject = 'Successful Submission'
+    recipient = quotes[1]  # Replace with the recipient's email address
+    body = 'Your request has been successfully submitted.'
+
+    send_email(subject, recipient, body)
+
+    return 'Success'
+    # return redirect(url_for('thank_you'))
+
+
+def send_email(subject, recipient, body):
+  msg = Message(subject,
+                sender='chdivakardiva192000@gmail.com',
+                recipients=[recipient])
+  msg.body = body
+  mail.send(msg)
+
+
+@app.route('/thank_you')
+def thank_you():
+  msg = Message('Hello from the other side!',
+                sender='chdivakardiva192000@gmail.com',
+                recipients=['chdivakardiva192000@gmail.com'])
+  msg.body = "Hey Paul, sending you this email from my Flask app, lmk if it works"
+  mail.send(msg)
+  return "Message sent!"
 
 
 if __name__ == '__main__':
