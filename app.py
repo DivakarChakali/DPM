a35456c7ccddb4b0ff380e591dfbaa1c55e48cc8
DivakarChakali <divakarchakali088@gmail.com>
2024-10-07 01:43:52 +0530

    added asthetic improvements to pages and addedd the bootstrap.min.css,bootstrap.bundle.min.js,new images, pages and custom styles.

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import re, os
 
 app = Flask(__name__)
 mail = Mail(app)
-app.secret_key = 'BD9C8CDB5A98EEDA'
+app.secret_key = os.environ.get('SECRET_KEY')
 
 # Simple email validation pattern
 email_pattern = r'^[\w\.-]+@[\w\.-]+$'
@@ -14,88 +14,100 @@ app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 465
 app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME')
 app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD')
-# app.config['MAIL_DEFAULT_SENDER'] = 'divakarpackers@gmail.com'
+app.config['MAIL_DEFAULT_SENDER'] = os.environ.get('DEFAULT_SENDER')
 app.config['MAIL_USE_TLS'] = False
 app.config['MAIL_USE_SSL'] = True
-
 
 @app.route('/')
 def home():
   return render_template('home.html')
 
-
 @app.route('/services')
 def services():
   return render_template('services.html')
 
+@app.route('/office-relocation')
+def office_relocation():
+  return render_template('office-relocation.html')
 
-@app.route('/aboutus')
+@app.route('/household-shifting')
+def household_shifting():
+  return render_template('household-shifting.html')
+
+@app.route('/loading-unloading')
+def loading_unloading():
+  return render_template('loading-unloading.html')
+
+@app.route('/packing-and-moving')
+def packing_moving():
+  return render_template('packing-moving.html')
+
+@app.route('/pre-moving-survey')
+def pre_moving_survey():
+  return render_template('pre-moving-survey.html')
+
+@app.route('/transportation')
+def transportation():
+  return render_template('transportation.html')
+
+@app.route('/request-quote')
+def request_quote():
+  return render_template('request-quote.html')
+
+@app.route('/about')
 def about():
-  return render_template('about_us.html')
+  return render_template('about.html')
 
-
-@app.route('/contactus')
-def contact():
-  return render_template('contact_us.html')
-
+@app.route('/contact-us')
+def contact_us():
+  return render_template('contact-us.html')
 
 @app.route('/faq')
 def faq():
   return render_template('faq.html')
 
+# @app.route('/submit-quote', methods=['POST'])
+# def submit_quote():
+#   name = request.form['name']
+#   email = request.form['email']
+#   phone = request.form['phone']
+#   moving_date = request.form['movingDate']
+#   origin = request.form['origin']
+#   destination = request.form['destination']
+#   special_requests = request.form['specialRequests']
 
-@app.route('/requestquote')
-def requestquote():
-  return render_template('request_quote.html')
+#   # Server-side validation
+#   if not name or not email or not phone or not moving_date or not origin or not destination:
+#     flash('All fields are required', 'error')
+#     return redirect(url_for('request_quote'))
 
+#   if not re.match(email_pattern, email):
+#     flash('Invalid email address', 'error')
+#     return redirect(url_for('request_quote'))
 
-@app.route('/submit_quote', methods=['POST'])
-def submit_quote():
-  name = request.form['name']
-  email = request.form['email']
-  phone = request.form['phone']
-  moving_date = request.form['movingDate']
-  origin = request.form['origin']
-  destination = request.form['destination']
-  special_requests = request.form['specialRequests']
+#   # Save the data to a database or perform further processing here
 
-  # Server-side validation
-  if not name or not email or not phone or not moving_date or not origin or not destination:
-    flash('All fields are required', 'error')
-    return redirect(url_for('request_quote'))
+#   # For this example, we'll just print the data
+#   print(f'Name: {name}')
+#   print(f'Email: {email}')
+#   print(f'Phone: {phone}')
+#   print(f'Moving Date: {moving_date}')
+#   print(f'Origin: {origin}')
+#   print(f'Destination: {destination}')
+#   print(f'Special Requests: {special_requests}')
 
-  if not re.match(email_pattern, email):
-    flash('Invalid email address', 'error')
-    return redirect(url_for('requestquote'))
+#   flash('Quote request submitted successfully', 'success')
+#   return redirect(url_for('request_quote'))
 
-  # Save the data to a database or perform further processing here
-
-  # For this example, we'll just print the data
-  print(f'Name: {name}')
-  print(f'Email: {email}')
-  print(f'Phone: {phone}')
-  print(f'Moving Date: {moving_date}')
-  print(f'Origin: {origin}')
-  print(f'Destination: {destination}')
-  print(f'Special Requests: {special_requests}')
-
-  flash('Quote request submitted successfully', 'success')
-  return redirect(url_for('requestquote'))
-
-
-quotes = []
-
-
-@app.route('/submit', methods=['POST'])
+@app.route('/submit-quote', methods=['POST'])
 def submit():
   # Get form data
   name = request.form['name']
   email = request.form['email']
   phone = request.form['phone']
-  moving_date = request.form['moving_date']
-  origin = request.form['origin']
-  destination = request.form['destination']
-  special_requests = request.form['special_requests']
+  moving_date = request.form['moving-date']
+  from_address = request.form['from-address']
+  to_address = request.form['to-address']
 
   # Server-side validation
   email_pattern = r'^[\w\.-]+@[\w\.-]+\.\w+$'
@@ -114,43 +126,30 @@ def submit():
   if errors:
     return render_template('home.html', errors=errors)
   else:
-    # Store the data or send it to your backend
-    quotes.append({
-        'name': name,
-        'email': email,
-        'phone': phone,
-        'moving_date': moving_date,
-        'origin': origin,
-        'destination': destination,
-        'special_requests': special_requests
-    })
     subject = 'Successful Submission'
-    recipient = quotes[1]  # Replace with the recipient's email address
     body = 'Your request has been successfully submitted.'
 
-    send_email(subject, recipient, body)
+    send_email(subject, email, body,from_address,to_address)
 
     return 'Success'
     # return redirect(url_for('thank_you'))
 
 
-def send_email(subject, recipient, body):
+def send_email(subject, recipient, body,from_address,to_address):
   msg = Message(subject,
                 sender='chdivakardiva192000@gmail.com',
                 recipients=[recipient])
   msg.body = body
   mail.send(msg)
 
-
-@app.route('/thank_you')
-def thank_you():
-  msg = Message('Hello from the other side!',
-                sender='chdivakardiva192000@gmail.com',
-                recipients=['chdivakardiva192000@gmail.com'])
-  msg.body = "Hey Paul, sending you this email from my Flask app, lmk if it works"
-  mail.send(msg)
-  return "Message sent!"
-
+# @app.route('/thank_you')
+# def thank_you():
+#   msg = Message('Hello from the other side!',
+#                 sender='chdivakardiva192000@gmail.com',
+#                 recipients=['chdivakardiva192000@gmail.com'])
+#   msg.body = "Hey Paul, sending you this email from my Flask app, lmk if it works"
+#   mail.send(msg)
+#   return "Message sent!"
 
 if __name__ == '__main__':
   app.run(host="0.0.0.0", debug=True)
